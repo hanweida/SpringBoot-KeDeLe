eb929846421c90b4b64987c1808e2eee8575c595
yingmule <534446340@qq.com>
2020-10-24 11:37:52 +0800

    Init spring cloud alibaba

---
+++ lab-02-springcloud-alibaba/src/main/java/com/kedele/springboot/lab/lab02springcloudalibaba/Lab02SpringcloudAlibabaApplication.java
@@ -0,0 +1,13 @@
+package com.kedele.springboot.lab.lab02springcloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab02SpringcloudAlibabaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab02SpringcloudAlibabaApplication.class, args);
+    }
+
+}
---
+++ lab-02-springcloud-alibaba/src/test/java/com/kedele/springboot/lab/lab02springcloudalibaba/Lab02SpringcloudAlibabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kedele.springboot.lab.lab02springcloudalibaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab02SpringcloudAlibabaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
