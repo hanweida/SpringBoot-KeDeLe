af23d2f07be5a984000ace695f54a0b35f582a9b
yingmule <534446340@qq.com>
2020-10-24 19:13:28 +0800

    Init spring cloud alibaba NaCOS

---
+++ labx-01-springcloud-alibaba-nacos/labx-01-springcloud-alibaba-nacos-discovery-provider/src/main/java/com/kedele/springboot/labs/provider/Labx01SpringcloudAlibabaNacosDiscoveryProviderApplication.java
@@ -0,0 +1,13 @@
+package com.kedele.springboot.labs.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labx01SpringcloudAlibabaNacosDiscoveryProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Labx01SpringcloudAlibabaNacosDiscoveryProviderApplication.class, args);
+    }
+
+}
---
+++ labx-01-springcloud-alibaba-nacos/labx-01-springcloud-alibaba-nacos-discovery-provider/src/test/java/com/kedele/springboot/labs/provider/Labx01SpringcloudAlibabaNacosDiscoveryProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kedele.springboot.labs.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labx01SpringcloudAlibabaNacosDiscoveryProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
