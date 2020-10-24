ff9f2fa8068caf0beec729ac4da43e65ff0bf63a
yingmule <534446340@qq.com>
2020-10-24 11:45:01 +0800

    Init spring cloud alibaba NaCOS

---
+++ labx-01-springcloud-alibaba-nacos/src/main/java/com/kedele/springboot/lab/labx01springcloudalibabanacos/Labx01SpringcloudAlibabaNacosApplication.java
@@ -0,0 +1,13 @@
+package com.kedele.springboot.lab.labx01springcloudalibabanacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labx01SpringcloudAlibabaNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Labx01SpringcloudAlibabaNacosApplication.class, args);
+    }
+
+}
